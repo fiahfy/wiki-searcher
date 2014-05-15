138f6c3f7f69d1d4eb4dc4e69a42c70f87bc2257
fiahfy <d.fiahfy@gmail.com>
2014-05-15 17:03:55 +0900

    add banner images

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -10,23 +10,48 @@ describe('controllers', function() {
 				return angular.equals(this.actual, expected);
 			}
 		});
+
+		var dummyStorage = {};
+		var dummyListener = null;
 		chrome = {
+			runtime: {
+				onMessage: {
+					addListener: function(){}
+				}
+			},
 			storage: {
 				local: {
-					get: function(key, callback){
-						callback();
-					}
+					get: function(){},
+					set: function(){}
 				}
 			}
 		};
-		spyOn(chrome.storage.local, 'get');
-	});
-
-	it('test1', function() {
-			expect(true).toBe(true);
+		spyOn(chrome.runtime.onMessage, 'addListener').andCallFake(function(callback){
+			dummyListener = callback;
+		});
+		spyOn(chrome.storage.local, 'get').andCallFake(function(key, callback){
+			callback(dummyStorage);
+		});
+		spyOn(chrome.storage.local, 'set').andCallFake(function(data, callback){
+			dummyStorage = data;
+			callback();
+		});
 	});
 
 	beforeEach(module('app'));
+
+	describe('BackgroundMainCtrl', function(){
+		var scope, ctrl, $httpBackend;
+
+		beforeEach(inject(function($rootScope, $controller) {
+			scope = $rootScope.$new();
+			ctrl = $controller('BackgroundMainCtrl', {$scope: scope});
+		}));
+
+		it('test', function() {
+			//
+		});
+	});
 
 	describe('OptionsMainCtrl', function(){
 		var scope, ctrl, $httpBackend;
@@ -36,9 +61,8 @@ describe('controllers', function() {
 			ctrl = $controller('OptionsMainCtrl', {$scope: scope});
 		}));
 
-
-		it('test2', function() {
-			expect(true).toBe(true);
+		it('test', function() {
+			//
 		});
 	});
 });
